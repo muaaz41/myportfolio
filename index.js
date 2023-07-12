6cd55d81095c753c342ee27f9e66d77dc06da1a0
muaaz41 <138881985+muaaz41@users.noreply.github.com>
2023-07-12 16:55:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function changeText() {
+    var textElement = document.getElementById("changeableText");
+    textElement.innerHTML = "BANDAY DA PUTTAR BAN OYE";
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    alert("HOGYA AB GAYIB HO!");
+}
+
